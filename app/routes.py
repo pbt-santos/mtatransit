a86c32ecafa6e37c3367ccc13eb0ee2d5bbab9de
pbt-santos <pbt.santos@outlook.com>
2020-07-09 00:57:29 +0100

    add rendering of first template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 # import the app variable from the init file (which is in app dir)
+from flask import render_template
 from app import app
 
 # Show this whenever we hit these two endpoints
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # Let's mock some turnstyle data. This will later come from a SQL database
+    turnstile = {'id': '1', 
+            'group': '2',
+            'turn_count': '5',
+            'turn_rate': '0.5'}
+    return render_template('index.html', title='Home', row=turnstile)
